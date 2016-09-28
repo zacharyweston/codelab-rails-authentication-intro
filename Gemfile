b68d92c555528ebb44b2df53ac70ac1e3a83a5e3
Zak Weston <zweston@MHC.local>
2016-09-28 12:49:05 -0400

    Prep for copy

--- Gemfile
+++ Gemfile
@@ -21,6 +21,9 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+#Added for TZ Info fix at MHC PC, 1230PM Wednesday Sept 28th, 2016
+gem 'tzinfo-data'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -33,7 +36,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-  # gem 'sqlite3'
+  gem 'sqlite3'
 end
 
 group :development do
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,19 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    if params[:title_search].present?
+      @books = Book.
+        where(on_shelf: true).
+        where('LOWER(title) LIKE LOWER(?)', "%#{params[:title_search]}%").
+        order(created_at: :desc)
+        # returns all the published posts where the value of
+        # `params[:title_search]` case-insensitively matches the
+        # title of the post, with results listed in descending
+        # order by when they were created (i.e. showing most
+        # recently created posts first)
+    else
+      @books = Book.all
+    end
   end
 
   # GET /books/1
